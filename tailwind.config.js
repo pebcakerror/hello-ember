fb5a06c01d2c54165538eff828788f020e54071a
pebcakerror <613574+pebcakerror@users.noreply.github.com>
2022-03-09 20:53:45 -0600

    add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  content: ['./app/components/**/*.hbs', './app/templates/**/*.hbs'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
